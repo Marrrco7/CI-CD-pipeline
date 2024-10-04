e3ea4ac75792cfdcf1c8c8c4198f93d2af655b14
Marco <marcodomenico.zv@gmail.com>
2024-10-04 23:30:06 +0200

    code

---
+++ src/main/java/com/microserviceapp/cicd_atcions/CicdAtcionsApplication.java
@@ -0,0 +1,28 @@
+package com.microserviceapp.cicd_atcions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class CicdAtcionsApplication {
+
+	@GetMapping(    "/greet")
+	public Map<String, String> greetUser() {
+		Map<String, String> response = new HashMap<>();
+		response.put("message", "Hello World!");
+		response.put("timestamp", LocalDateTime.now().toString());
+		return response;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAtcionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microserviceapp/cicd_atcions/CicdAtcionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserviceapp.cicd_atcions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAtcionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
