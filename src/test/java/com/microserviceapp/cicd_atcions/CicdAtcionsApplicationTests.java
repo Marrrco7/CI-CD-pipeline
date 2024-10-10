4d5e1bf5bcbc9ea515d08427bff7ca8407d13dbf
Marco <marcodomenico.zv@gmail.com>
2024-10-10 16:17:47 +0200

    implemented test

--- src/test/java/com/microserviceapp/cicd_atcions/CicdAtcionsApplicationTests.java
+++ src/test/java/com/microserviceapp/cicd_atcions/CicdAtcionsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.microserviceapp.cicd_atcions;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CicdAtcionsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private TestRestTemplate restTemplate;
 
+	@Test
+	void testGreetEndpoint() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/greet", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isNotEmpty();
+	}
 }
